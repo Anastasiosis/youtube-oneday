14be9d11d723dc08bbd58ac93197fd70c21670f9
Anastasios <anastasios@hotmail.co.za>
2020-06-06 19:16:22 +0100

    initial xcode project creation

---
+++ youtube-oneday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-oneday
+//
+//  Created by Ricky on 06/06/2020.
+//  Copyright © 2020 Tasso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
